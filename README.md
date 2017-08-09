300186f819292bd4f86a7e8f22ca20d534976793
MacroPolo <riddell91@gmail.com>
2017-08-09 21:00:07 +0100

    barebones HeightMap class for manipulating raster data

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # real-terrain
-Generate 16-bit heightmaps from the USGS data services
+Generate 16-bit heightmaps from the USGS data services.
+
+Aimed at game developers who want quick but high quality heightmaps.
--- real-terrain.py
+++ real-terrain.py
@@ -1,6 +1,7 @@
 # USGS elevation data: https://viewer.nationalmap.gov/basic/
 
 import argparse
+import re
 import requests
 import sys
 import subprocess
@@ -9,17 +10,44 @@ GDAL_INFO_BIN = 'bin/gdalinfo.exe'
 GDAL_TRANSLATE_BIN = 'bin/gdal_translate.exe'
 
 input_file = 'input/arcgrid_test'
-output_file = 'output/arcgrid_test.tif'
+output_tif = 'output/arcgrid_test.tif'
+output_png = 'output/arcgrid_test.png'
 # input_file = 'input/img_test.img'
 # output_file = 'output/img_test.tif'
 
-def translate_to_geotiff():
-    gdal_translate_geotiff = [GDAL_TRANSLATE_BIN, '-co', 'COMPRESS=LZW', '-co', 
-                              'PREDICTOR=3', '-co', 'TFW=YES', '-of', 'GTiff', 
-                              input_file, output_file]
-    subprocess.call(gdal_translate_geotiff)
+class HeightMap(object):
+    def __init__(self):
+        self.min_value = '432.098'
+        self.max_value = '713.806'
 
-# gdal_info_ = [GDAL_INFO_BIN, '-stats', 'input/img_test.img']
-# subprocess.call(gdal_info_cmd)
+    def raster_to_geotiff(self):
+        gdal_translate_cmd = [GDAL_TRANSLATE_BIN, '-co', 'COMPRESS=LZW', '-co', 
+                                'PREDICTOR=3', '-co', 'TFW=YES', '-of', 'GTiff', 
+                                input_file, output_tif]
+        subprocess.call(gdal_translate_cmd)
 
-translate_to_geotiff()
+    def gdal_info(self):
+        gdal_info_cmd = [GDAL_INFO_BIN, '-stats', output_tif]
+        try:
+            raster_info = subprocess.check_output(gdal_info_cmd)
+        except subprocess.CalledProcessError as e:
+            print("Failed to retrieve raster information:\n", e.output)
+        return raster_info
+
+    def geotiff_to_png(self, res='2048'):
+        gdal_translate_cmd = [GDAL_TRANSLATE_BIN, '-outsize', res, res, 
+                                '-of', 'PNG', '-ot', 'UInt16', '-scale', 
+                                self.min_value, self.max_value, '0', '65535', 
+                                input_file, output_png]
+        subprocess.call(gdal_translate_cmd)
+
+
+def find_limits(raster_info):
+    re.compile()
+    re.findall()
+
+h = HeightMap()
+
+h.raster_to_geotiff()
+raster_info = h.gdal_info()
+h.geotiff_to_png()
