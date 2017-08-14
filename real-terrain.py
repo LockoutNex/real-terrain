47276fa756134eb85b790262ba25397acad62238
MacroPolo <riddell91@gmail.com>
2017-08-14 20:33:37 +0100

    Simplify gdal_translate to handle geotiff input. Translate directly from input (IMG, ArcGrid, GeoTIFF to PNG

--- real-terrain.py
+++ real-terrain.py
@@ -14,68 +14,55 @@ OUTPUT_DIR = 'output/'
 parser = argparse.ArgumentParser(description='Generate a 16-bit PNG ' \
                                              'heightmap from USGS data')
 parser.add_argument('data', metavar='input_data', 
-                    help='Name of the input data (IMG or ArcGrid)')
-parser.add_argument('-r', metavar='output_resolution', type=str,
-                    help='Resolution of the final output (default 4096x4096)')
+                    help='Name of the input data (IMG, ArcGrid or GeoTiff format)')
+parser.add_argument('-r', metavar='output_resolution', type=str, default='4096',
+                    help='Resolution of the final heightmap (default 4096x4096)')
 args = parser.parse_args()
+res = args.r
+input_data = args.data
 
 ts = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-input_data = INPUT_DIR + args.data
-output_tif = OUTPUT_DIR + ts + '_geotiff.tif'
-output_png = OUTPUT_DIR + ts + '_heightmap.png'
-res = args.r
 
 class HeightMap(object):
     def __init__(self, resolution):
         self.res = resolution
-        if self.res is None:
-            self.res = '4096'
-        else:
-            self.res = resolution
         self.raster_info = ''
         self.min_elevation = ''
         self.max_elevation = ''
+        self.ts = ts
+        self.input_data = INPUT_DIR + input_data
+        self.output_png = OUTPUT_DIR + self.ts + '_heightmap.png'
 
     def generate(self):
-        self.raster_to_geotiff()
         self.gdal_info()
         self.find_elevation_range()
-        self.geotiff_to_png()
+        self.raster_to_png()
 
-    def raster_to_geotiff(self):
-        gdal_translate_cmd = [GDAL_TRANSLATE_BIN, '-co', 'COMPRESS=LZW', '-co', 
-                                'PREDICTOR=3', '-co', 'TFW=YES', '-of', 'GTiff', 
-                                input_data, output_tif]
-        print('>>> Translating raster data to GeoTIFF')
-        subprocess.call(gdal_translate_cmd)
-        
     def gdal_info(self):
-        gdal_info_cmd = [GDAL_INFO_BIN, '-stats', output_tif]
+        gdal_info_cmd = [GDAL_INFO_BIN, '-stats', self.input_data]
         try:
-            print('\n>>> Extracting elevation information')
+            print('\n>>> Extracting elevation information from input data')
             self.raster_info = str(subprocess.check_output(gdal_info_cmd))
         except subprocess.CalledProcessError as e:
             print("Failed to retrieve raster information:\n", e.output)
             sys.exit(1)
         
     def find_elevation_range(self):
-        self.min_elevation = re.findall(r'(?<=STATISTICS_MINIMUM=)\d{1,}', 
+        self.min_elevation = re.findall(r'(?<=STATISTICS_MINIMUM=)\-?\d{1,}', 
                                         self.raster_info)[0]
-        self.max_elevation = re.findall(r'(?<=STATISTICS_MAXIMUM=)\d{1,}', 
+        self.max_elevation = re.findall(r'(?<=STATISTICS_MAXIMUM=)\-?\d{1,}', 
                                         self.raster_info)[0]
-        print('-------------------------------------------------------')
         print('Minimum elevation: {}m'.format(self.min_elevation))
         print('Maximum elevation: {}m'.format(self.max_elevation))
         print('Elevation range: {}m'.format(int(self.max_elevation) - 
                                            int(self.min_elevation)))
-        print('-------------------------------------------------------')
         
-    def geotiff_to_png(self):
-        gdal_translate_cmd = [GDAL_TRANSLATE_BIN, '-outsize', self.res, self.res, 
-                                '-of', 'PNG', '-ot', 'UInt16', '-scale', 
-                                self.min_elevation, self.max_elevation, 
-                                '0', '65535', input_data, output_png]
-        print('\n>>> Converting GeoTiff to 16-bit PNG')
+    def raster_to_png(self):
+        gdal_translate_cmd = [GDAL_TRANSLATE_BIN, '-outsize', self.res, 
+                              self.res, '-of', 'PNG', '-ot', 'UInt16', '-scale', 
+                              self.min_elevation, self.max_elevation, '0', '65535', 
+                              self.input_data, self.output_png]
+        print('\n>>> Converting input data to 16-bit PNG')
         subprocess.call(gdal_translate_cmd)
 
 h = HeightMap(resolution=res)
